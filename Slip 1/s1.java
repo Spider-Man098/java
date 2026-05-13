53204a84449229a2a573b5fab62439c823461ce0
SpiderMan <25_durvankur.kansare@ges-coengg.org>
2026-05-13 20:28:18 +0530

    slip1.java

--- Slip 1/s1.java
+++ Slip 1/s1.java
@@ -1,19 +1,34 @@
-class s1 {
-public static void main(String args[]) {
-int a = Integer.parseInt(args[0]);
-int b = Integer.parseInt(args[1]);
-System.out.println("Sum = " + (a + b));
-if(a > b)
-System.out.println("Largest = " + a);
-else
-System.out.println("Largest = " + b);
-if(a % 2 == 0)
-System.out.println(a + " is Even");
-else
-System.out.println(a + " is Odd");
-if(b % 2 == 0)
-System.out.println(b + " is Even");
-else
-System.out.println(b + " is Odd");
-}
+class slip1  {
+	public static void main(String args[]) {
+	int a = Integer.parseInt(args[0]);
+        int b = Integer.parseInt(args[1]);
+	System.out.println(" Sum = " + ( a + b ) );
+
+       if(a > b ) {  
+	System.out.println(" A is greater than B " );
+	}
+	else if ( a < b ) {
+	System.out.println( " B is greater than A " );
+	}
+  if ( a % 2 == 0 ) {
+	System.out.println( " A is even " ) ;
+	}
+	else {
+	System.out.println( " A is odd " ) ;
+	}
+	if ( b % 2 == 0 ) {
+	System.out.println( " B is even " ) ;
+	}
+	else {
+	System.out.println( " B is odd " ) ;
+	}
+	try {
+		int div = a/b;
+		System.out.println("Division = " + div );
+	}
+		catch (ArithmeticException e ) {
+		System.out.println("Cannot divide by zero");
+	}
+
+	}
 }
