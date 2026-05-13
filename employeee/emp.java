88ef072403c0b590d30c0aec022df9c2a709d920
SpiderMan <25_durvankur.kansare@ges-coengg.org>
2026-05-13 22:04:48 +0530

    Update emp.java

--- employeee/emp.java
+++ employeee/emp.java
@@ -1,38 +1,51 @@
-package employeee;
+package employe;
 
-import java.util.Scanner;
-
-public class emp {
-
-    int id;
+public class emp
+{
+    // Data members
     String name;
-    double salary;
+    int empid;
+    String category;
+    double basicpay, hra, da, allowance;
+    double grosspay, pf, incometax, netpay;
 
-    Scanner sc = new Scanner(System.in);
-
-    public void getData() {
-
-        System.out.print("Enter ID: ");
-        id = sc.nextInt();
-
-        sc.nextLine();
-
-        System.out.print("Enter Name: ");
-        name = sc.nextLine();
-
-        System.out.print("Enter Salary: ");
-        salary = sc.nextDouble();
+    // Method to assign values
+    public void getData(String n, int id, String c, double bp)
+    {
+        name = n;
+        empid = id;
+        category = c;
+        basicpay = bp;
     }
 
-    public void calculate() {
+    // Method to calculate salary
+    public void calculate()
+    {
+        hra = basicpay * 0.20;
+        da = basicpay * 0.10;
+        allowance = basicpay * 0.05;
 
-        salary = salary;
+        grosspay = basicpay + hra + da + allowance;
+
+        pf = basicpay * 0.12;
+        incometax = grosspay * 0.10;
+
+        netpay = grosspay - pf - incometax;
     }
 
-    public void display() {
-
-        System.out.println("ID: " + id);
-        System.out.println("Name: " + name);
-        System.out.println("Salary: " + salary);
+    // Method to display details
+    public void display()
+    {
+        System.out.println("Employee Name : " + name);
+        System.out.println("Employee ID : " + empid);
+        System.out.println("Category : " + category);
+        System.out.println("Basic Pay : " + basicpay);
+        System.out.println("HRA : " + hra);
+        System.out.println("DA : " + da);
+        System.out.println("Allowance : " + allowance);
+        System.out.println("Gross Pay : " + grosspay);
+        System.out.println("Provident Fund : " + pf);
+        System.out.println("Income Tax : " + incometax);
+        System.out.println("Net Pay : " + netpay);
     }
 }
