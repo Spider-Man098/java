2373dcd54cc20c2cff1c583abd33575e2d0d2d19
SpiderMan <25_durvankur.kansare@ges-coengg.org>
2026-05-14 18:30:49 +0530

    Update emp.java

--- employeee/emp.java
+++ employeee/emp.java
@@ -1,51 +1,60 @@
 package employeee;
 
+import java.util.Scanner;
+
 public class emp
 {
-    // Data members
-    String name;
-    int empid;
-    String category;
-    double basicpay, hra, da, allowance;
-    double grosspay, pf, incometax, netpay;
+    int empId;
+    String empName, category;
 
-    // Method to assign values
-    public void getData(String n, int id, String c, double bp)
+    double basic, hra, da, allowance, pf, incomeTax, gross, net;
+
+    public void getData()
     {
-        name = n;
-        empid = id;
-        category = c;
-        basicpay = bp;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter Employee ID: ");
+        empId = sc.nextInt();
+        sc.nextLine();
+
+        System.out.print("Enter Employee Name: ");
+        empName = sc.nextLine();
+
+        System.out.print("Enter Employee Category: ");
+        category = sc.nextLine();
+
+        System.out.print("Enter Basic Pay: ");
+        basic = sc.nextDouble();
     }
 
-    // Method to calculate salary
     public void calculate()
     {
-        hra = basicpay * 0.20;
-        da = basicpay * 0.10;
-        allowance = basicpay * 0.05;
+        hra = basic * 0.20;
+        da = basic * 0.50;
+        allowance = basic * 0.10;
 
-        grosspay = basicpay + hra + da + allowance;
+        pf = basic * 0.12;
+        incomeTax = basic * 0.05;
 
-        pf = basicpay * 0.12;
-        incometax = grosspay * 0.10;
-
-        netpay = grosspay - pf - incometax;
+        gross = basic + hra + da + allowance;
+        net = gross - (pf + incomeTax);
     }
 
-    // Method to display details
     public void display()
     {
-        System.out.println("Employee Name : " + name);
-        System.out.println("Employee ID : " + empid);
-        System.out.println("Category : " + category);
-        System.out.println("Basic Pay : " + basicpay);
-        System.out.println("HRA : " + hra);
-        System.out.println("DA : " + da);
-        System.out.println("Allowance : " + allowance);
-        System.out.println("Gross Pay : " + grosspay);
-        System.out.println("Provident Fund : " + pf);
-        System.out.println("Income Tax : " + incometax);
-        System.out.println("Net Pay : " + netpay);
+        System.out.println("\n----- Employee Salary Details -----");
+
+        System.out.println("Employee ID       : " + empId);
+        System.out.println("Employee Name     : " + empName);
+        System.out.println("Employee Category : " + category);
+
+        System.out.println("Basic Pay         : " + basic);
+        System.out.println("HRA               : " + hra);
+        System.out.println("DA                : " + da);
+        System.out.println("Allowance         : " + allowance);
+        System.out.println("PF                : " + pf);
+        System.out.println("Income Tax        : " + incomeTax);
+        System.out.println("Gross Pay         : " + gross);
+        System.out.println("Net Pay           : " + net);
     }
 }
